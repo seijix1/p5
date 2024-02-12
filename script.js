fe51c61ab909564b9a4023fcdd169829f7fd02e3
seijix1 <sergio.mastro03@gmail.com>
2024-02-12 11:48:40 +0100

    fixed index script and style

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fadingLight = document.querySelector(".fading-light");
+    let opacity = 0;
+    let fadeSpeed = 0.005; // Customize the fading speed
+    
+    function fadeInOut() {
+      fadingLight.style.opacity = opacity;
+      opacity += fadeSpeed;
+      if (opacity > 1 || opacity < 0) {
+        fadeSpeed *= -1;
+      }
+      requestAnimationFrame(fadeInOut);
+    }
+    
+    fadeInOut();
+});
