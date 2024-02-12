94c65678a53ceb1668f2772f1d16b336430beba1
seijix1 <sergio.mastro03@gmail.com>
2024-02-12 12:40:00 +0100

    Added second star, orange border to header

--- script.js
+++ script.js
@@ -13,4 +13,25 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     fadeInOut();
+
+    
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fadingLight = document.querySelector(".fading-light2");
+    let opacity = 0;
+    let fadeSpeed = 0.005; // Customize the fading speed
+    
+    function fadeInOut() {
+      fadingLight.style.opacity = opacity;
+      opacity += fadeSpeed;
+      if (opacity > 1 || opacity < 0) {
+        fadeSpeed *= -1;
+      }
+      requestAnimationFrame(fadeInOut);
+    }
+    
+    fadeInOut();
+
+    
 });
