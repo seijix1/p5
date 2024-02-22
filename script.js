12af3ad17d53a8a9a512896f8f30c93eb070fe62
seijix1 <sergio.mastro03@gmail.com>
2024-02-22 19:12:09 +0100

    "Better" backgrounds, added scroll

--- script.js
+++ script.js
@@ -36,8 +36,28 @@ document.addEventListener("DOMContentLoaded", function() {
     
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const fadingLight = document.querySelector(".fading-light3");
+  let opacity = 0;
+  let fadeSpeed = 0.0049; // Customize the fading speed
+  
+  function fadeInOut() {
+    fadingLight.style.opacity = opacity;
+    opacity += fadeSpeed;
+    if (opacity > 1 || opacity < 0) {
+      fadeSpeed *= -1;
+    }
+    requestAnimationFrame(fadeInOut);
+  }
+  
+  fadeInOut();
+
+  
+});
+
 var chain = $("#chains")[0];
 $(".img-wrapper").mouseenter(function() {
     chain.currentTime = 0;
 		chain.play();
 });
+
